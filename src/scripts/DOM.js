3234706d22bb7d18fbae569b8a4b67611a9fa37b
Bartue <[bartbatiancila28@gmail.com]>
2024-11-10 21:04:26 +0800

    Remove the statusBtns event listeners

--- src/scripts/DOM.js
+++ src/scripts/DOM.js
@@ -53,30 +53,7 @@ export function createDOM() {
     return btn;
   }
 
-  //test event listener ---------------------------------------
-  Object.values(statusBtns).forEach(btn => {
-    btn.addEventListener("click", (event) => { 
-      event.preventDefault();
   
-      document.querySelectorAll(".my-project-element").forEach(content => {
-        content.classList.remove("active");
-      });
-
-      Object.values(statusBtns).forEach(btn => {
-        btn.removeAttribute("class");
-      });
-
-      btn.classList.add("active");
-  
-      if(mainCon.contains(mainProjectCon)){
-        mainCon.removeChild(mainProjectCon);
-      }else {
-        console.log("Main project container not exist yet!");
-        
-      }
-    });
-  
-  });
   //projects DOM (bottom part of the sidebar) -----------------------------------
 
   //project container
