f43a5044b858c95455b5a547e98fcd0e34fd9ca7
Bartue <[bartbatiancila28@gmail.com]>
2024-11-15 09:25:26 +0800

    import createMain module. Call all the necessary methods from modules

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import "./styles/sidebar.css";
 import "./styles/footer.css";
 import "./styles/main.css";
 import { sidebarProject } from "./scripts/project";
-import { DOM } from "./scripts/DOM";
+import { createMain } from "./scripts/main";
 import { sidebarStatus } from "./scripts/status";
-/* import { main } from "./scripts/main"; */
+
+
+sidebarProject.renderListOfProjects();
+sidebarProject.projectState();
+createMain.renderTaskList();
+sidebarStatus.statusBtnsState();
 
